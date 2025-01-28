b21f3c15354b1f606d3a3cd9eaef83cf741ae9c3
Daisuke171 <deadmanwonderland171@gmail.com>
2025-01-28 19:17:22 -0300

    se creo la funcion listarAmigos()

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ function agregarAmigo(){
     }else{
         amigos.push(amigoInput);
         limpiarCaja('#amigo');
+        listarAmigos();
     }
     ///console.log(amigos);
-
-    
     return;
+}
+
+function listarAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++){
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
 }
